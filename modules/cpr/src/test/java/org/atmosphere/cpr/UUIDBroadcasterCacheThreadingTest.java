96a3c304d9f466c4964f33c3e828a843e52b2ea1
Tatu Lund <tatu@vaadin.com>
2021-05-10 17:23:47 +0300

    Code formatting

--- modules/cpr/src/test/java/org/atmosphere/cpr/UUIDBroadcasterCacheThreadingTest.java
+++ modules/cpr/src/test/java/org/atmosphere/cpr/UUIDBroadcasterCacheThreadingTest.java
@@ -26,13 +26,13 @@ public class UUIDBroadcasterCacheThreadingTest {
         cache.configure(config);
 
         Thread t = new Thread(new Runnable() {
-			@Override
-			public void run() {
-	            for (int i = 0; i < NUM_MESSAGES; i++) {
-	                BroadcastMessage broadcastMessage = createBroadcastMessage();
-	                cache.addToCache(BROADCASTER_ID, CLIENT_ID, broadcastMessage);
-	            }
-			}        	
+            @Override
+	    public void run() {
+	        for (int i = 0; i < NUM_MESSAGES; i++) {
+	            BroadcastMessage broadcastMessage = createBroadcastMessage();
+	            cache.addToCache(BROADCASTER_ID, CLIENT_ID, broadcastMessage);
+	        }
+            }
         });
         t.start();
 
