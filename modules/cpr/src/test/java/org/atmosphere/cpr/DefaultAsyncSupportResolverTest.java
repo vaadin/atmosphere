01d647dacc52e8e192614fc8de7c041b3f79ec50
Tatu Lund <tatu@vaadin.com>
2022-12-13 13:20:35 +0200

    Fix import non static

--- modules/cpr/src/test/java/org/atmosphere/cpr/DefaultAsyncSupportResolverTest.java
+++ modules/cpr/src/test/java/org/atmosphere/cpr/DefaultAsyncSupportResolverTest.java
@@ -29,7 +29,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers;
+import org.mockito.ArgumentMatchers;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.spy;
 
